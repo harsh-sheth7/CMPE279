805ed59ed7cf53d98982e32826b8d1de220b81fd
Shadow279 <hhhand123@gmail.com>
2021-11-11 15:54:59 -0800

    Server changes

--- Assignment1/server.c
+++ Assignment1/server.c
@@ -24,17 +24,17 @@ int main(int argc, char const *argv[])
     }
 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-	&opt, sizeof(opt)))
+                   &opt, sizeof(opt)))
     {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
     if (bind(server_fd, (struct sockaddr *)&address,
-	sizeof(address)) < 0)
+             sizeof(address)) < 0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -47,16 +47,46 @@ int main(int argc, char const *argv[])
     }
 
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
+                             (socklen_t *)&addrlen)) < 0)
     {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    valread = read(new_socket, buffer, 1024);
-    printf("Read %d bytes: %s\n", valread, buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+    pid_t childProcess = fork();
+    if (childProcess < 0)
+    {
+        perror("Fork Process Failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (childProcess == 0)
+    {
+        if (setuid(65534) < 0)
+        {
+            perror("Previlege Separation Failed");
+            exit(EXIT_FAILURE);
+        }
+        valread = read(new_socket, buffer, 1024);
+        if (valread < 0)
+        {
+            perror("Read Failed");
+            exit(EXIT_FAILURE);
+        }
+        printf("Read %d bytes: %s\n", valread, buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+    }
+    else
+    {
+        int status = 0;
+        while ((wait(&status)) > 0)
+            ;
+    }
+
+    // valread = read(new_socket, buffer, 1024);
+    // printf("Read %d bytes: %s\n", valread, buffer);
+    // send(new_socket, hello, strlen(hello), 0);
+    // printf("Hello message sent\n");
 
     return 0;
 }
